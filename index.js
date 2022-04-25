821b9aea3e707758e4594466e4857d93c3b42637
ErynKim <eryn_kim@naver.com>
2022-04-25 13:52:42 +0900

    첫서버

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 // 엔트리파일
-console.log("hello world");
+// console.log("hello world");
+// var a =3;
 
-var a =3;
+var express = require("express") // node_modules > express의 모듈을 가져옴
+var app = express() // app은 객체
+
+// 사용자가 localhost 5000 hello URL 주소로 접속했을 때 
+app.get("/hello", (req, res) => {
+    res.send("hello world!!") // 응답을 브라우저로 보내줌
+})
+
+// 맨 밑에 놓기
+app.listen(5000, () => {
+    console.log("sever is running on port 5000!")
+}) 
